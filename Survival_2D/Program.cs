5ea035658d07a031fc2ad6083254e73bf18b93b1
Nils Berglund <nilsberglund04@gmail.com>
2022-03-30 19:10:24 +0200

    commit2

--- Survival_2D/Program.cs
+++ Survival_2D/Program.cs
@@ -8,27 +8,31 @@ Raylib.SetTargetFPS(60);
 int HP = 100;
 int water = 10;
 int food = 10;
-int heat = 3;
 
-float speed = 3f;
+float speed = 2f;
 
 Vector2 textPos = new Vector2(800, 300);
 
 Texture2D playerImage = Raylib.LoadTexture("pixelGuy_Survival2D.png");
+Texture2D enemyImage = Raylib.LoadTexture("monster_Survival2D.png");
 Texture2D backgroundImage = Raylib.LoadTexture("camp_Survival2D.png");
+Texture2D backgroundImage2 = Raylib.LoadTexture("lake_Survival2D.png");
 
 Rectangle playerRect = new Rectangle(600, 400, playerImage.width, playerImage.height);
-Rectangle campRect = new Rectangle(0,0, backgroundImage.width, backgroundImage.height);
+Rectangle campRect = new Rectangle(0, 0, backgroundImage.width, backgroundImage.height);
+Rectangle lakeRect = new Rectangle(0, 0, backgroundImage2.width, backgroundImage2.height);
 Rectangle campFireRect = new Rectangle(400, 400, 200, 100);
 Rectangle waterRect = new Rectangle(200, 300, 200, 100);
+
+Rectangle enemyRect = new Rectangle(0,0, enemyImage.width, enemyImage.height);
 
 string room = "camp";
 
 while(!Raylib.WindowShouldClose())
 {
-    if(room == "camp" || room == "lake")
+    if(room == "camp")
     {
-         Vector2 movement = ReadMovement(speed);
+        Vector2 movement = ReadMovement(speed);
 
             playerRect.x += movement.X;
             playerRect.y += movement.Y;
@@ -39,31 +43,61 @@ while(!Raylib.WindowShouldClose())
             }
             if(playerRect.y < 0 || playerRect.y + playerRect.height > Raylib.GetScreenHeight())
             {
-                 playerRect.y -= movement.Y;
+                room = "lake";
+
+                playerRect.y = 0;
             }
-    }
-    if(room == "camp")
-    {
+
         if(Raylib.CheckCollisionRecs(playerRect, campFireRect))
         {
-
+            
         }
+    }
+    else if(room == "lake")
+    {
+        Vector2 movement = ReadMovement(speed);
 
+            playerRect.x += movement.X;
+            playerRect.y += movement.Y;
+
+            if(playerRect.x < 0 || playerRect.x + playerRect.width > Raylib.GetScreenWidth())
+            {
+                playerRect.x -= movement.X;
+            }
+            if(playerRect.y < 0 || playerRect.y + playerRect.height > Raylib.GetScreenHeight())
+            {
+                room = "camp";
+
+                playerRect.x = 480;
+                playerRect.y = Raylib.GetScreenHeight() - playerRect.height;
+            }
+    }
+    if(room == "camp" || room == "lake")
+    {
+        Vector2 movement = ReadMovement(speed);
+
+            playerRect.x += movement.X;
+            playerRect.y += movement.Y;
     }
 
 
     Raylib.BeginDrawing();
 
-    if(room == "camp" || room == "lake")
+    if(room == "camp")
     {
         Raylib.DrawTexture(backgroundImage, (int)campRect.x, (int)campRect.y, Color.WHITE);
-        Raylib.DrawTexture(playerImage, (int)playerRect.x, (int)playerRect.y, Color.WHITE);
-        Raylib.DrawText(HP.ToString(), 10, 10, 20, Color.WHITE);
-        Raylib.DrawText(water.ToString(), 40, 10, 20, Color.WHITE);
-        Raylib.DrawText(food.ToString(), 60, 10, 20, Color.WHITE);
-        Raylib.DrawText(heat.ToString(), 80, 10, 20, Color.WHITE);
     }
-
+    else if(room =="lake")
+    {
+        Raylib.DrawTexture(backgroundImage2, (int)lakeRect.x, (int)lakeRect.y, Color.WHITE);
+    }
+    if(room == "camp" || room == "lake")
+    {
+        Raylib.DrawTexture(playerImage, (int)playerRect.x, (int)playerRect.y, Color.WHITE); 
+        Raylib.DrawText(HP.ToString(), 50, 10, 25, Color.WHITE);
+        Raylib.DrawText(water.ToString(), 150, 10, 25, Color.WHITE);
+        Raylib.DrawText(food.ToString(), 225, 10, 25, Color.WHITE);
+    }
     Raylib.EndDrawing();
 
     static Vector2 ReadMovement(float speed)
